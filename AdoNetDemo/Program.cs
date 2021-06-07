d74a8d45646a9eac22d6d5151e7ec38f2d5c3db7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-06-07 21:29:54 +0300

    Add files via upload

---
+++ AdoNetDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(
+                "Server=127.0.0.1\\root;Database=soft_uni;Integrated Security=true"))
+            {
+                
+                sqlConnection.Open();
+                string command = "SELECT COUNT (*) FROM Employees";
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                object result = sqlCommand.ExecuteScalar();
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
