0a126177f351a8a0690c105c3638eca1e03c215e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-06-29 19:31:21 +0300

    Add files via upload

---
+++ AdoApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AdoApp1
+{
+    public class Program
+    {
+        private const string ConnectionString = @"Server=DESKTOP-79AOHR2;Database=MinionsDB;Integrated Security=true;";
+
+        public static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+
+            string minionTownQueryText = @"SELECT Id FROM Towns WHERE [Name] = 'Plovdiv'";
+            using SqlCommand minionTownCommand = new SqlCommand(minionTownQueryText, sqlConnection);
+
+            string minionTownId = minionTownCommand.ExecuteScalar().ToString();
+
+            Console.WriteLine(minionTownId);
+        }
+    }
+}
