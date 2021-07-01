b532ab06045f2674e1aab09b0ab8c901e0ddf0b4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-02 01:41:06 +0300

    Add files via upload

---
+++ DbFirstTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Text;
+
+namespace DbFirstTests.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MinionsDBContext minionsDB = new MinionsDBContext();
+
+            var superPowerVillains = minionsDB.Villains.Where(x => x.EvilnessFactor.Name == "Super evil").ToList();
+
+            foreach (var spv in superPowerVillains)
+            {
+                Console.WriteLine(spv.Name);
+            }
+        }
+    }
+}
