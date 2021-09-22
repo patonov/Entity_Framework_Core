287552af967213f6ef83e2c38a69101708d517ec
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-22 18:43:26 +0300

    Add files via upload

---
+++ EFdemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using EFdemo.Models;
+
+namespace EFdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            Console.WriteLine("Total number of projects {0}.", db.Projects.Count());
+
+            foreach (Project p in db.Projects)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            Console.WriteLine("Number of projects whose title starts with ML {0}.", db.Projects.Count(x => x.Name.StartsWith("ML")));
+
+            Console.WriteLine("They are as follow:");
+
+            foreach (Project p in db.Projects.Where(x => x.Name.StartsWith("ML")))
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+}
