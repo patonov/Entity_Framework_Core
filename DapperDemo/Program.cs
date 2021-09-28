8212ac2926361dab40adae5badceac39d6a5ef86
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-28 13:14:27 +0300

    Add files via upload

---
+++ DapperDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Server=DESKTOP-79AOHR2;Database=SoftUni;Integrated Security=true"))
+            {
+                var projects = connection.Query<ProjectInfo>("SELECT Name, ProjectID FROM Projects WHERE ProjectID > 100");
+
+                foreach (var project in projects)
+                {
+                    Console.WriteLine(project.Name);
+                }
+            }
+        }
+    }
+
+    public class ProjectInfo
+    { 
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    
+    }
+        
+}
