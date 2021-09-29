3299286e69a81ee5b134249915e0ac272dca1548
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-29 21:33:18 +0300

    Add files via upload

---
+++ LinqDemoPlus/LinqDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Z.EntityFramework.Plus;
+
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            var dbContext = new MinionsDBContext();
+            dbContext.Towns.Where(x => x.Id <= 3).DeleteAsync();
+
+
+
+        }
+    }
+}
