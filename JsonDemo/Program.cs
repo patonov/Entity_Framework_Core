1553b696befe01486b9198740ee7ebbffe8a71dd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-09 18:13:55 +0300

    Add files via upload

---
+++ JsonDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace JsonDemo
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        
+        public int TemperatureC { get; set; } = 30;
+        
+        public string Summary { get; set; } = "Hot summer day";
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var weather = new WeatherForecast();
+
+            string json = JsonSerializer.Serialize(weather);
+
+            File.WriteAllText("weather.json", json);
+
+            Console.WriteLine(json);
+
+            var jsonString = File.ReadAllText("weather.json");
+            var weatherResult = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+            
+
+        }
+    }
+}
