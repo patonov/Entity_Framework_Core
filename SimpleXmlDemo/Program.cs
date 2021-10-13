b8684fa3cb5988e13d7c195d694029035501c3a3
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-13 23:26:48 +0300

    Add files via upload

---
+++ SimpleXmlDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace SimpleXmlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var xml = File.ReadAllText("Planes.xml");
+
+            XDocument xDocument = XDocument.Load("Planes.xml");
+
+            // Console.WriteLine(xDocument);
+
+            Console.WriteLine(xDocument.Root.Elements().Count());
+
+            Console.WriteLine(xDocument.Root.Elements().FirstOrDefault(x => x.Element("color").Value == " Blue "));
+
+            Console.WriteLine(xDocument.Root.Elements().FirstOrDefault(x => x.Element("color").Value == " Blue ").Element("year").Value);
+        }
+    }
+}
